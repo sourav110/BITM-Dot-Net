5ca9e346a7de7c80f36feb491ec754548ac61502
ShimuSourav <rdfox.007@gmail.com>
2019-10-17 01:24:59 +0600

    Web form connection with DB [Sql server]

---
+++ Day11/StudentInfoManagementApp/StudentInfoManagementApp/Models/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentInfoManagementApp.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public string RegNo { get; set; }
+        public string Email { get; set; }
+        public string MobileNo { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+
+        public Student(string studentName, string regNo, string email, string mobileNo, int age, string address)
+        {
+            this.StudentName = studentName;
+            this.RegNo = regNo;
+            this.Email = email;
+            this.MobileNo = mobileNo;
+            this.Age = age;
+            this.Address = address;
+        }
+    }
+}
---
+++ Day11/StudentInfoManagementApp/StudentInfoManagementApp/StudentInfoUI.aspx.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using StudentInfoManagementApp.Models;
+
+namespace StudentInfoManagementApp
+{
+    public partial class StudentInfoUI : System.Web.UI.Page
+    {
+        public object SSqlConnection { get; private set; }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ShowAllStudentInfo();
+        }
+       
+
+        protected void saveButton_Click(object sender, EventArgs e)
+        {
+            string studentName = inputStudentName.Value;
+            string regNo = inputRegNo.Value;
+            string email = inputEmail.Value;
+            string mobileNo = inputMobile.Value;
+            int age = Convert.ToInt32(inputAge.Value);
+            string address = inputAddress.Value;
+
+            Student student = new Student(studentName, regNo, email, mobileNo, age, address);
+
+            string connectionString = @"Server = RDFOX; Database = StudentInfoDB; Integrated Security = true;";
+
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            string query = "INSERT INTO Student_tbl (StudentName, RegNo, Email, MobileNo, Age, Address) VALUES('"+student.StudentName+"', '"+student.RegNo+"', '"+student.Email+"', '"+student.MobileNo+"', "+student.Age+" , '"+student.Address+"')";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            connection.Open();
+            int rowEffect = command.ExecuteNonQuery();
+            connection.Close();
+
+            if(rowEffect > 0)
+            {
+                messageLabel.Text = String.Empty;
+                messageLabel.Text = "Studenet info saved!";
+                messageLabel.ForeColor = System.Drawing.Color.Green;
+                ClearField();
+                ShowAllStudentInfo();
+            }
+
+            else
+            {
+                messageLabel.Text = String.Empty;
+                messageLabel.Text = "Failed to saved!";
+                messageLabel.ForeColor = System.Drawing.Color.Red;
+            }
+
+        }
+
+        private void ShowAllStudentInfo()
+        {
+            List<Student> studentList = new List<Student>();
+
+            string connectionString = @"Server = RDFOX; Database = StudentInfoDB; Integrated Security = true;";
+
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            string query = "SELECT * FROM Student_tbl ORDER BY StudentID DESC";
+            SqlCommand command = new SqlCommand(query, connection);
+
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                int studentId = (int)reader["StudentId"];
+                string studentName = reader["StudentName"].ToString();
+                string regNo = reader["RegNo"].ToString();
+                string email = reader["Email"].ToString();
+                string mobileNo = reader["MobileNo"].ToString();
+                int age = (int)reader["Age"];
+                string address = reader["Address"].ToString();
+
+                Student student = new Student(studentName, regNo, email, mobileNo, age, address);
+                student.StudentId = studentId;
+
+                studentList.Add(student);
+            }
+
+            reader.Close();
+            connection.Close();
+
+            StudentGridView.DataSource = studentList;
+            StudentGridView.DataBind();
+        }
+
+
+        protected void ClearField()
+        {
+            inputStudentName.Value = String.Empty;
+            inputRegNo.Value = String.Empty;
+            inputEmail.Value = String.Empty;
+            inputMobile.Value = String.Empty;
+            inputAge.Value = String.Empty;
+            inputAddress.Value = String.Empty;
+        }
+    }
+}
