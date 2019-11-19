89887db343c7068c11ffe6b8143c8f0de2146394
ShimuSourav <rdfox.007@gmail.com>
2019-11-19 15:15:59 +0600

    creating view from a created controller

---
+++ SelfPractice/HelloWorldInMVC/HelloWorldInMVC/Controllers/StudentController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWorldInMVC.Controllers
+{
+    public class StudentController : Controller
+    {
+        //public string Result(string name, string address, int? roadNo=98)
+        //{
+        //    return name + ", Your result is A+ & address is " + roadNo + ", " + address;
+        //}
+
+        public ActionResult Result()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ SelfPractice/HelloWorldInMVC/HelloWorldInMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HelloWorldInMVC.MvcApplication" Language="C#" %>
