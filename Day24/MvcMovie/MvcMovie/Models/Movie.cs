7f4cb08910da0e672917d509d4399c45a61b6def
ShimuSourav <rdfox.007@gmail.com>
2019-11-19 23:11:30 +0600

    creating DB with entity framework

---
+++ Day24/MvcMovie/MvcMovie/Models/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int MovieId { get; set; }
+        public string Name { get; set; }
+        public string ReleaseYear { get; set; }
+        public double Price { get; set; }
+    }
+}
