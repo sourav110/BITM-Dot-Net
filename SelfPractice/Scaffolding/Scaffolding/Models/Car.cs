b923fce72b76b0590c2d55b41791fefe398324f9
ShimuSourav <rdfox.007@gmail.com>
2019-11-21 01:42:06 +0600

    scaffolding- Entity framework

---
+++ SelfPractice/Scaffolding/Scaffolding/Models/Car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Scaffolding.Models
+{
+    public class Car
+    {
+        public int CarId { get; set; }
+        public string Name { get; set; }
+        public string RegNo { get; set; }
+        public double Price { get; set; }
+    }
+}
