f3a006aaea9e385e37130104d085b4d1db23663d
ShimuSourav <rdfox.007@gmail.com>
2019-11-21 23:17:25 +0600

    student EF

---
+++ SelfPractice/StudentInfoMVC/StudentInfoMVC/Models/StudentDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace StudentInfoMVC.Models
+{
+    public class StudentDbContext : DbContext
+    {
+        public DbSet<Student> students { get; set; }
+    }
+}
