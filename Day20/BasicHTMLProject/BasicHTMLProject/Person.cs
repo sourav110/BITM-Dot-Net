65c77cd325e7c05d02be3fabdd35a4f673a331cd
ShimuSourav <rdfox.007@gmail.com>
2019-11-28 13:25:20 +0600

    hands on Jquery

---
+++ Day20/BasicHTMLProject/BasicHTMLProject/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BasicHTMLProject
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string  LastName { get; set; }
+
+        public string GetFullName()
+        {
+            return FirstName + " " + MiddleName + " " + LastName;
+        }
+    }
+}
---
+++ Day20/BasicHTMLProject/BasicHTMLProject/person.js
@@ -0,0 +1,10 @@
+﻿
+var person = function (firstName, middleName, lastName) {
+    this.FirstName = firstName;
+    this.MiddleName = middleName;
+    this.LastName = lastName;
+
+    this.GetFullName = function () {
+        return this.FirstName + " " + this.MiddleName + " " + this.LastName;
+    }
+};
