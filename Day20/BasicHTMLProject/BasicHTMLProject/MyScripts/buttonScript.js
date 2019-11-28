b320469aad6a88a4918c9b6e8fc68907be2ea89e
ShimuSourav <rdfox.007@gmail.com>
2019-11-28 14:57:25 +0600

    form validation by JQuery

---
+++ Day20/BasicHTMLProject/BasicHTMLProject/MyScripts/buttonScript.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+    $('#btn').click(function () {
+        $('.banner').toggle(1000)
+    })
+
+    $('#btn').hover(function () {
+        $('body').css("background-color", "red");
+    }, function () {
+        $('body').css("background-color", "white");
+    })
+
+})
