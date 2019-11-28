34d4afb47218bf2401f6330c203e239ee056a01c
ShimuSourav <rdfox.007@gmail.com>
2019-11-28 15:21:26 +0600

    form validation script by JQuery

---
+++ Day20/HandsOnJquery/HandsOnJquery/MyScripts/formValidation.js
@@ -0,0 +1,10 @@
+﻿$('#login-form').submit(function () {
+    var name = $('#name').val();
+    if (name == "") {
+        alert("please enter name");
+        return false;
+    }
+    else {
+        alert("your name is " + name);
+    }
+})
