d8e93273d44d12f7563ebed128dd21903a337722
ShimuSourav <rdfox.007@gmail.com>
2019-12-04 12:15:35 +0600

    advance html helper

---
+++ Day30/AdvanceHelperAndDataAnnotation/AdvanceHelperAndDataAnnotation/Models/Department.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AdvanceHelperAndDataAnnotation.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string DeptName { get; set; }
+    }
+}
