6bd2968fa0c29861b44981e595e7215a1208afcb
ShimuSourav <rdfox.007@gmail.com>
2019-12-08 11:30:31 +0600

    CF migration

---
+++ Day32/CFMigrationAndHTMLhelper/CFMigrationAndHTMLhelper/Models/Department.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+
+namespace CFMigrationAndHTMLhelper.Models
+{
+    public class Department
+    {
+        [Key]
+        public int DepartmentId { get; set; }
+
+        [Required(ErrorMessage ="Please enter department code")]
+        [StringLength(7, MinimumLength =2, ErrorMessage ="Code must be 2-7 characters long")]
+        [Column(TypeName ="Varchar")]
+        [Display(Name ="Code")]
+        public string DepartmentCode { get; set; }
+
+        [Required(ErrorMessage = "Please enter department name")]
+        [StringLength(50)]
+        [Column(TypeName = "Varchar")]
+        [Display(Name = "Name")]
+        public string DepartmentName { get; set; }
+    }
+}
---
+++ Day32/CFMigrationAndHTMLhelper/CFMigrationAndHTMLhelper/Models/ProjectDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace CFMigrationAndHTMLhelper.Models
+{
+    public class ProjectDbContext : DbContext
+    {
+        public DbSet<Department> Departments { get; set; }
+    }
+}
