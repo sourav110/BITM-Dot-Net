cd5d9e34d3c06178ffbeaeb33e499664463c3485
ShimuSourav <rdfox.007@gmail.com>
2019-12-08 12:08:11 +0600

    html helper controller

---
+++ Day32/CFMigrationAndHTMLhelper/CFMigrationAndHTMLhelper/Controllers/DepartmentsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CFMigrationAndHTMLhelper.Models;
+
+namespace CFMigrationAndHTMLhelper.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        // GET: Departments
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Department department)
+        {
+            return View();
+        }
+
+    }
+}
